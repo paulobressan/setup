f68ed15443006cc91847ad3f4bcb00e8f7ee95d0
paulobressan <paulo.bressan@outlook.com>
2024-10-25 18:06:20 -0300

    feat: added current lsp attached in lua line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -25,7 +25,8 @@ return {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
+        -- lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = { "vim.lsp.buf_get_clients()[1].name", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" }
       },
