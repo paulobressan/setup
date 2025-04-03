cb6c540ed3b7a5c5e46b436f8e0894a2f9468184
paulobressan <paulo.bressan@outlook.com>
2025-04-03 14:41:14 -0300

    feat: updated plugin catppuccin to theme

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    enabled = true,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    enabled = false,
+    config = function()
+      vim.cmd.colorscheme "gruvbox"
+    end
+  }
+}
