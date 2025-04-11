c4be054aee58fb7e0a574a19a602f0bb75455449
paulobressan <paulo.bressan@outlook.com>
2025-04-11 15:43:04 -0300

    feat: updated theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
   {
     "catppuccin/nvim",
     priority = 1000,
-    enabled = true,
+    enabled = false,
     config = function()
       vim.cmd.colorscheme "catppuccin"
     end
@@ -18,9 +18,12 @@ return {
   {
     "sainnhe/gruvbox-material",
     priority = 1000,
-    enabled = false,
+    enabled = true,
     config = function()
       vim.cmd.colorscheme "gruvbox-material"
+      vim.cmd.hi "Normal guibg=#141617 ctermbg=NONE"
+      vim.cmd.hi "NormalFloat guibg=#1d2021 guifg=NONE"
+      vim.cmd.hi "FloatBorder guibg=#1d2021 guifg=NONE "
     end
   }
 }
