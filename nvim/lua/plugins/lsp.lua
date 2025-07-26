1cc51a8e97bc08e3e56927817df1d9b743afd41b
paulobressan <paulo.bressan@outlook.com>
2025-07-26 17:39:44 -0300

    feat: updated ts lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 local servers        = { "lua_ls", "jsonls", "yamlls", "html", "terraformls", "gopls" }
-local manual_servers = { "ts_ls", "vue_ls", "rust_analyzer" }
+local manual_servers = { "vtsls", "vue_ls", "rust_analyzer" }
 local formatters     = { "prettier" }
 local combine        = vim.tbl_flatten
 
@@ -21,6 +21,7 @@ return {
       })
     end,
     dependencies = {
+      "neovim/nvim-lspconfig",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
       "williamboman/mason-lspconfig.nvim",
     }
@@ -30,7 +31,7 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local on_attach    = function(_, bufnr)
+      local on_attach = function(_, bufnr)
         local map = function(mode, lhs, rhs)
           vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true })
         end
@@ -52,49 +53,83 @@ return {
         end
       end
 
-      local on_init      = function(client, _)
+      local on_init = function(client, _)
         if client.supports_method("textDocument/semanticTokens") then
           client.server_capabilities.semanticTokensProvider = nil
         end
       end
 
+      local lsp = vim.lsp
+
       -- Auto servers
       for _, name in ipairs(servers) do
-        vim.lsp.config(name, {
+        lsp.config(name, {
           on_attach    = on_attach,
           on_init      = on_init,
           capabilities = capabilities,
         })
       end
-      vim.lsp.enable(servers)
+      lsp.enable(servers)
 
-      -- TypeScript + Vue
-      local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server" ..
-        "/node_modules/@vue/language-server")
-      vim.lsp.config("ts_ls", {
+      lsp.config("vue_ls", {
         on_attach    = on_attach,
         on_init      = on_init,
         capabilities = capabilities,
-        init_options = {
-          plugins = {
-            {
-              name      = "@vue/typescript-plugin",
-              location  = vue_ls_path,
-              languages = { "vue" },
+        handlers     = {
+          ["tsserver/request"] = function(_, result, context)
+            local clients = lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+            if #clients == 0 then
+              vim.notify('Could not find `vtsls`, `vue_ls` requires it.', vim.log.levels.ERROR)
+              return
+            end
+
+            local ts_client = clients[1]
+            local param = result[1] -- Fix unpack
+            local id, command, payload = table.unpack(param)
+
+            ts_client:exec_cmd({
+              title = 'vue_request_forward',
+              command = 'typescript.tsserverRequest',
+              arguments = { command, payload },
+            }, { bufnr = context.bufnr }, function(_, r)
+              local response_data = { { id, r.body } }
+              context.client.notify('tsserver/response', response_data)
+            end)
+          end,
+        },
+      })
+      lsp.enable("vue_ls")
+
+      local vue_language_server_path = vim.fn.expand("$MASON/packages/vue-language-server" ..
+        "/node_modules/@vue/language-server")
+      local vue_plugin = {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+        configNamespace = 'typescript',
+      }
+
+      lsp.config("vtsls", {
+        on_attach    = on_attach,
+        on_init      = on_init,
+        capabilities = capabilities,
+        settings     = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = { vue_plugin },
             },
           },
         },
-        filetypes    = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        filetypes    = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
       })
-      vim.lsp.enable("ts_ls")
-      vim.lsp.enable("vue_ls")
+      lsp.enable("vtsls")
 
       -- Rust Analyzer
       local features = {}
       for feat in (os.getenv("LSP_RUST_FEATURES") or ""):gmatch("([^,]+)") do
         table.insert(features, vim.trim(feat))
       end
-      vim.lsp.config("rust_analyzer", {
+      lsp.config("rust_analyzer", {
         on_attach    = on_attach,
         on_init      = on_init,
         capabilities = capabilities,
@@ -105,13 +140,13 @@ return {
           },
         },
       })
-      vim.lsp.enable("rust_analyzer")
+      lsp.enable("rust_analyzer")
     end,
   },
   {
     "j-hui/fidget.nvim",
     config = function()
       require("fidget").setup()
-    end
+    end,
   }
 }
